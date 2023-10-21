0b7985245639c88ed24f678fcda50f1977bc491f
Varun Prohit <20163@iiitu.ac.in>
2023-10-21 13:37:34 +0530

    added connect wallet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Contract, ethers} from 'ethers'
+import { useEffect, useState } from 'react';
+import instance from '../src/MyNft.sol/MyNFT.json'
 function App() {
+
+  const contractaddress = "0x143995657415F561983d2112B65061a74B402EA5"; //to be fetch from .env
+  console.log(contractaddress)
+  const [state,setstate]= useState({
+    signer:null,
+    contract:null
+  }) 
+  const abi = instance.abi;
+  console.log(abi)
+  useEffect(()=>{
+    const instancecreate=async()=>{
+      try{if(window.ethereum==null){
+        alert("download metamask");
+      }
+      else{
+        // console.log(contractaddress)
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
+        const contract = new Contract(contractaddress,abi,signer)
+        setstate({
+          signer,
+          contract
+        })
+        // console.log(signer.address)
+      }}catch(error){
+        console.log(error)
+      }
+    }
+    instancecreate();
+    }
+    ,[])
+   
+    
   return (
     <div className="App">
       <header className="App-header">
